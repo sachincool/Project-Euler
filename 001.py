6fe7bee4ae0cef12e803525b7bdd938ba0aea87c
Harshit  Luthra <love.coolsachin@gmail.com>
2018-06-15 10:36:53 +0530

    better approach

--- 001.py
+++ 001.py
@@ -9,3 +9,13 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 print(sum(x for x in range(1000) if (x%3==0 or x%5==0)))
+
+// Better Approach 
+ n = int(input().strip())-1
+    n1=n//3
+    n1=3*n1*(n1+1)//2
+    n2=n//5
+    n2=5*n2*(n2+1)//2
+    n3=n//15
+    n3=15*n3*(n3+1)//2
+    print (n1+n2-n3)
